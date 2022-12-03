3806b206609d686dc86a2d02db3a17a024afca91
Igor-S-M <igorsousa3148@gmail.com>
2022-12-03 02:25:26 -0300

    feat: getChoice

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -1,13 +1,25 @@
 import { choiceCollection } from "../database/index.js";
 
-export default async function postChoice(req, res) {
+export async function postChoice(req, res) {
   const choiceData = res.locals.choiceData;
 
   try {
-    await choiceCollection.insertOne(choiceData)
-    res.sendStatus(200)
+    await choiceCollection.insertOne(choiceData);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
-    res.sendStatus(500)
+    res.sendStatus(500);
+  }
 }
+
+export async function getChoice(req, res) {
+  const { id } = req.params;
+
+  try {
+    const choiceList = await choiceCollection.find({ pollId: id }).toArray();
+    res.status(200).send(choiceList);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
--- src/routes/choiceRoutes.js
+++ src/routes/choiceRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import postChoice from "../controllers/choice.controller.js";
+import {getChoice, postChoice} from "../controllers/choice.controller.js";
 import validatePostChoice from "../middleware/choiceValidation.middleware.js";
 
 const choiceRouter = express.Router();
 
 choiceRouter.post("/choice", validatePostChoice, postChoice)
+choiceRouter.get("/poll/:id/choice", getChoice)
 
 export default choiceRouter
